7968526d4e4f2880dcb910bfeddf5ca3c92132a5
Khuyen-Ng <yykhuyenn27@gmail.com>
2025-05-19 11:52:52 +0700

    update-k

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -26,6 +26,7 @@ class CartController extends Controller
     $order->name = $validatedData['name'];
     $order->phone = $validatedData['phone'];
     $order->address = $validatedData['address'];
+       //discount neu can
       //$payment_method 
     $order->status = 'ordered';
     $order->total = Session::get('checkout')['total'];
@@ -39,5 +40,15 @@ class CartController extends Controller
     $orderItem->quantity = $item->qty;
     $orderItem->save();
 }
+    }
+   public function order_confirmation()
+    {
+        if (Session::has('order_id')) {
+            $order = Order::find(Session::get('order_id'));
+            if ($order) {
+                return view('order_confirmation', compact('order'));
+            }
+        }
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController; // Import controller-K
+use App\Http\Controllers\OrderItemController;// Import controller-K
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Auth::routes();
@@ -13,4 +14,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 //Xử lý đơn hàng
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::get('/order-confirmation', [CartController::class, 'order_confirmation'])->name('cart.order.confirmation');
 Route::get('/order',[OrderController::class,'orders'])->name('orders');
+Route::get('/order/{order_id}/details' ,[OrderItemController::class,'order_details'])->name('order.details');
